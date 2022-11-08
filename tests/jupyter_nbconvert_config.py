63763961a2c22163c086086413acbbbb17cc47b5
tonyfast <tony.fast@gmail.com>
2022-11-08 09:58:48 -0800

    im in the desert fixing these tests

--- tests/jupyter_nbconvert_config.py
+++ tests/jupyter_nbconvert_config.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 ASIDE = ".jp-InputArea-prompt, .prompt, .jp-OutputArea-prompt"
 
 def asides(object, resources):
-    soup = BeautifulSoup(object, features="lxml")
+    soup = BeautifulSoup(object, features="html")
     for node in soup.select(ASIDE):
         node.name = "aside"
     return str(soup)
