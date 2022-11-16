36125b0f74025736d3a5616f7c696c0ec7869dcf
tonyfast <tony.fast@gmail.com>
2022-11-15 20:56:43 -0800

    relative link

--- dodo.py
+++ dodo.py
@@ -28,17 +28,25 @@ def task_export_html(notebooks):
             target = notebook.with_suffix(f".{format}.html")
             output = DOCS / target
             cmd = f"jupyter nbconvert --to={format} --output={target.name} --output-dir={output.parent} {notebook}"
-            yield dict(name=f"export_html:{format}:{notebook}", actions=[cmd], targets=[target], file_dep=[notebook])
+            yield dict(
+                name=f"export_html:{format}:{notebook}",
+                actions=[cmd],
+                targets=[target],
+                file_dep=[notebook],
+            )
             rel.append(output)
 
     INDEX = DOCS / "index.md"
     body = "# sample converted notebooks\n\n"
     for id in rel:
+        id = id.relative_to("docs")
         body += "* "
-        body += F"[notebook](https://github.com/Iota-School/notebooks-for-all/blob/main/{id}) "
-        body += F"[html](/notebooks-for-all/{id.relative_to('docs')}) "
+        body += f"[notebook](https://github.com/Iota-School/notebooks-for-all/blob/main/{id}) "
+        body += f"[html](/notebooks-for-all/{id}) "
         body += str(id)
         body += "\n"
-    yield dict(name=F"export_html:{INDEX}", targets=[INDEX], actions=[
-        (lambda x: INDEX.write_text(x) and None, [body])
-    ])
+    yield dict(
+        name=f"export_html:{INDEX}",
+        targets=[INDEX],
+        actions=[(lambda x: INDEX.write_text(x) and None, [body])],
+    )
