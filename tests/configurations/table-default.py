0f09b38866b2d49609acb386f11801eded5c005b
tonyfast <tony.fast@gmail.com>
2023-04-05 17:22:34 -0700

    configure semantic table pygments

--- tests/configurations/table-default.py
+++ tests/configurations/table-default.py
@@ -1,4 +1,5 @@
 """export notebooks as  table of forms"""
 
+c.CSSHTMLHeaderPreprocessor.style = "github-light-colorblind"
 c.NbConvertApp.export_format = "nbconvert_html5.form_exporter.FormExporter"
 c.CSSHTMLHeaderPreprocessor.style = "default"
