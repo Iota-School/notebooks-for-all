541493c1f9e7275101374801f8991e0ed1535f1f
tonyfast <tony.fast@gmail.com>
2023-11-13 20:06:59 -0800

    finally add an assertion to the tests

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1,13 +1,10 @@
-from json import dumps
+from json import dumps, loads
 from logging import getLogger
 from pathlib import Path
-from requests_cache import install_cache
-import requests
 from test_nbconvert_html5 import exporter
 
-install_cache("a11y-audit")
 
-from pytest import mark
+from pytest import fixture, mark
 
 HERE = Path(__file__).parent
 NOTEBOOKS = HERE / "notebooks"
@@ -16,6 +13,12 @@ HTML =  EXPORTS / "html"
 LOGGER = getLogger(__name__)
 AUDIT = EXPORTS / "audit"
 
+@fixture
+def axe():
+    from requests_cache import install_cache
+    import requests
+    install_cache("a11y-audit")
+    yield requests.get("https://cdn.jsdelivr.net/npm/axe-core").text
 
 @mark.parametrize(
     "notebook",
@@ -25,9 +28,9 @@ AUDIT = EXPORTS / "audit"
         if x.name not in {"Imaging_Sky_Background_Estimation.ipynb"}
     ),
 )
-def test_baseline_a11y_template(page, exporter, notebook):
+def test_baseline_a11y_template(page, exporter, notebook, axe):
     config = {}
-    config.setdefault("runOnly", ["best-practice", "wcag22aa", "wcag2aaa"])
+    config.setdefault("runOnly", ["best-practice", "wcag22aa", "wcag22a"])
     config.setdefault("allowedOrigins", ["<same_origin>"])
     target = HTML / notebook.with_suffix(".html").name
     target.parent.mkdir(exist_ok=True, parents=True)
@@ -36,10 +39,12 @@ def test_baseline_a11y_template(page, exporter, notebook):
     
     page.goto(Path.as_uri(target))
     LOGGER.debug(f"""injecting axe into {target.name}""")
-    page.evaluate(requests.get("https://cdn.jsdelivr.net/npm/axe-core").text)
+    page.evaluate(axe)
     LOGGER.debug(f"""auditting {target.name} with axe""")
     result = page.evaluate(f"window.axe.run(window.document, {dumps(config)})")
     AUDIT.mkdir(parents=True, exist_ok=True)
     audit = AUDIT / notebook.with_suffix(".json").name
     LOGGER.info(f"""writing {audit} with {len(result["violations"])} violations""")
     audit.write_text(dumps(result))
+
+    assert not result["violations"], result
