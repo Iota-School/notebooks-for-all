c7cb8e8d460c236002ba27999afbbf0e97a932ff
tonyfast <tony.fast@gmail.com>
2023-11-14 11:22:57 -0800

    exclude mathjax objects in audit

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -37,11 +37,14 @@ def test_baseline_a11y_template(page, exporter, notebook, axe):
     LOGGER.debug(f"""injecting axe into {target.name}""")
     target.write_text(exporter.from_filename(notebook)[0])
     
+    test = dict(
+        exclude=["#MathJax_Message"]
+    )
     page.goto(Path.as_uri(target))
     LOGGER.debug(f"""injecting axe into {target.name}""")
     page.evaluate(axe)
     LOGGER.debug(f"""auditting {target.name} with axe""")
-    result = page.evaluate(f"window.axe.run(window.document, {dumps(config)})")
+    result = page.evaluate(f"window.axe.run({dumps(test)}, {dumps(config)})")
     AUDIT.mkdir(parents=True, exist_ok=True)
     audit = AUDIT / notebook.with_suffix(".json").name
     LOGGER.info(f"""writing {audit} with {len(result["violations"])} violations""")
