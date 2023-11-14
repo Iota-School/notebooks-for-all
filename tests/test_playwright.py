76cd4093b23c6f84ef5430d8f569db4ac4028078
tonyfast <tony.fast@gmail.com>
2023-11-14 12:31:38 -0800

    use exceptiongroups to collect axe reults

--- tests/test_playwright.py
+++ tests/test_playwright.py
@@ -1,6 +1,9 @@
+import dataclasses
 from json import dumps, loads
 from logging import getLogger
 from pathlib import Path
+
+import exceptiongroup
 from test_nbconvert_html5 import exporter
 
 
@@ -9,16 +12,56 @@ from pytest import fixture, mark
 HERE = Path(__file__).parent
 NOTEBOOKS = HERE / "notebooks"
 EXPORTS = HERE / "exports"
-HTML =  EXPORTS / "html"
+HTML = EXPORTS / "html"
 LOGGER = getLogger(__name__)
 AUDIT = EXPORTS / "audit"
+
+# ignore mathjax at the moment. we might be able to turne mathjax to have better
+# accessibility. https://github.com/Iota-School/notebooks-for-all/issues/81
+MATHJAX = "[id^=MathJax]"
+
+
+@dataclasses.dataclass
+class AxeException(Exception):
+    message: str
+    target: list
+    data: dict = dataclasses.field(repr=False)
+
+    types = {}
+
+    @classmethod
+    def new(cls, id, impact, message, data, target, **kwargs):
+        if id in cls.types:
+            cls = cls.types.get(id)
+        else:
+            cls = cls.types.setdefault(
+                id,
+                type(
+                    f"{impact.capitalize()}{''.join(map(str.capitalize, id.split('-')))}Exception",
+                    (cls,),
+                    dict(),
+                ),
+            )
+        return cls(message, target, data)
+
+    @classmethod
+    def from_violations(cls, data):
+        out = []
+        for violation in (violations := data.get("violations")):
+            for node in violation["nodes"]:
+                for exc in node["any"]:
+                    out.append(cls.new(**exc, target=["target"]))
+        return exceptiongroup.ExceptionGroup(f"{len(violations)} accessibility violations", out)
+
 
 @fixture
 def axe():
     from requests_cache import install_cache
     import requests
+
     install_cache("a11y-audit")
     yield requests.get("https://cdn.jsdelivr.net/npm/axe-core").text
+
 
 @mark.parametrize(
     "notebook",
@@ -30,16 +73,17 @@ def axe():
 )
 def test_baseline_a11y_template(page, exporter, notebook, axe):
     config = {}
-    config.setdefault("runOnly", ["best-practice", "wcag22aa", "wcag22a"])
+    config.setdefault(
+        "runOnly",
+        ["act", "best-practice", "experimental", "wcag21a", "wcag21aa", "wcag22aa", "wcag2aaa"],
+    )
     config.setdefault("allowedOrigins", ["<same_origin>"])
     target = HTML / notebook.with_suffix(".html").name
     target.parent.mkdir(exist_ok=True, parents=True)
     LOGGER.debug(f"""injecting axe into {target.name}""")
     target.write_text(exporter.from_filename(notebook)[0])
-    
-    test = dict(
-        exclude=["#MathJax_Message"]
-    )
+
+    test = dict(exclude=[MATHJAX])
     page.goto(Path.as_uri(target))
     LOGGER.debug(f"""injecting axe into {target.name}""")
     page.evaluate(axe)
@@ -50,4 +94,5 @@ def test_baseline_a11y_template(page, exporter, notebook, axe):
     LOGGER.info(f"""writing {audit} with {len(result["violations"])} violations""")
     audit.write_text(dumps(result))
 
-    assert not result["violations"], result
+    if result["violations"]:
+        raise AxeException.from_violations(result)
