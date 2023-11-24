014b382e267878d7d98763954936431524fb3fc9
tonyfast <tony.fast@gmail.com>
2023-11-23 19:21:45 -0800

    remove user testing research notes from the project

--- convert.py
+++
@@ -1,9 +0,0 @@
-from nbconvert import get_exporter
-
-exporter = get_exporter("html5")
-
-out = exporter().from_filename("tests/notebooks/lorenz.ipynb")
-
-
-with open("tests/out.html", "w") as out_file:
-    out_file.write(str(out[0]))
